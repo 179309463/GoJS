c8299c7a861acc99d9942ec2064d69a65fda4e93
fengzhuojia <fengzhuojia@appledeMacBook-Air.local>
2017-05-01 11:56:04 +0800

    remove all google relation files

--- samples/assets/WebGL/webgl-utils.js
+++ samples/assets/WebGL/webgl-utils.js
@@ -1,5 +1,4 @@
 /*
- * Copyright 2010, Google Inc.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -12,7 +11,6 @@
  * copyright notice, this list of conditions and the following disclaimer
  * in the documentation and/or other materials provided with the
  * distribution.
- *     * Neither the name of Google Inc. nor the names of its
  * contributors may be used to endorse or promote products derived from
  * this software without specific prior written permission.
  *
